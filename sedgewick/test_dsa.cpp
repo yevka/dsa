d39e50bd3b617ed4a1d0f226e1d88d6b40af2108
yevka <yevka.vladyslav@gmail.com>
2021-01-15 14:57:49 +0200

    add program 1.2, quick union

--- sedgewick/test_dsa.cpp
+++ sedgewick/test_dsa.cpp
@@ -6,14 +6,15 @@
 
 
 template<int N = 10>
-class Graph {
+class Graph1 {
 public:
-    Graph() : id(N, 0) {
+    Graph1() : id(N, 0) {
         for(int i = 0; i < N ; ++i)
             id[i] = i;
     }
 
     bool insert(int p, int q) {
+        // quick find
         if (!find(p, q)) {
             union_(p, q);
             return false;
@@ -26,9 +27,6 @@ private:
 
 private:
     bool find(int p, int q) {
-        // 0 1 2 3 4 5 6 7 8 9 - id
-        // 0 1 2 3 4 5 6 7 8 9 - 1
-        // 0 1 2 4 4 5 6 7 8 9 - 2
         return id[p] == id[q];
     }
 
@@ -42,7 +40,34 @@ private:
     }
 };
 
-TEST_CASE("program 1.1: connectivity problem", "sedgewick") {
+
+template<int N = 10>
+class Graph2 {
+public:
+    Graph2() : id(N, 0) {
+        for(int i = 0; i < N ; ++i)
+            id[i] = i;
+    }
+
+    bool insert(int p, int q) {
+        // quick union
+        int i = 0, j = 0;
+        for(i = p; i != id.at(i) ; i = id[i]);
+        for(j = q; j != id.at(j) ; j = id[j]);
+        bool is_found = (i == j);
+        if (!is_found)
+            id[i] = j;
+        return is_found;
+    }
+
+private:
+    std::vector<int> id;
+};
+
+
+
+
+TEST_CASE("connectivity problem", "sedgewick") {
     std::vector<std::pair<int, int>> pairs = {
             {3, 4},
             {4, 9},
@@ -61,27 +86,42 @@ TEST_CASE("program 1.1: connectivity problem", "sedgewick") {
     };
 
     const std::vector<int> is_connect = {
-             false // 3 4
-            ,false // 4 9
-            ,false // 8 0
-            ,false // 2 3
-            ,false // 5 6
-            ,true  // 2 9
-            ,false // 5 9
-            ,false // 7 3
-            ,false // 4 8
-            ,true  // 5 6
-            ,true  // 0 2
-            ,false // 6 1
-            ,true  // 0 8
-            ,false // 10 1
+              false // 3 4
+            , false // 4 9
+            , false // 8 0
+            , false // 2 3
+            , false // 5 6
+            , true  // 2 9
+            , false // 5 9
+            , false // 7 3
+            , false // 4 8
+            , true  // 5 6
+            , true  // 0 2
+            , false // 6 1
+            , true  // 0 8
+            , false // 10 1
     };
 
-    Graph<11> g;
-    for(int i = 0; i < pairs.size() ; ++i) {
-        int p = pairs.at(i).first;
-        int q = pairs.at(i).second;
-        std::cout << p << " " << q << std::endl;
-        REQUIRE(is_connect.at(i) == g.insert(p, q));
+    SECTION( "quick_find" ) {
+        Graph1<11> g;
+        for(int i = 0; i < pairs.size() ; ++i) {
+            const int p = pairs.at(i).first;
+            const int q = pairs.at(i).second;
+            std::cout << p << " " << q << std::endl;
+            REQUIRE(is_connect.at(i) == g.insert(p, q));
+        }
     }
+
+    std::cout << std::endl;
+
+    SECTION( "quick_union" ) {
+        Graph2<11> g;
+        for(int i = 0; i < pairs.size() ; ++i) {
+            const int p = pairs.at(i).first;
+            const int q = pairs.at(i).second;
+            std::cout << p << " " << q << std::endl;
+            REQUIRE(is_connect.at(i) == g.insert(p, q));
+        }
+    }
+
 }
